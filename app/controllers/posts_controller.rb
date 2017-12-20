9977c997670aa1c1e7aa8dcd9c3df86c14bf1e10
Samreen Malla <samreen.malla@gmail.com>
2017-12-20 14:16:37 +0545

    edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,21 @@ end
   end
 end
 
+
+def edit
+  @post = Post.find(params[:id])
+end
+
+def update
+  @post = Post.find(params[:id])
+  @post.update(post_params)
+  if @post.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
+
 private
 
 def post_params
