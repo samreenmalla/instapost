ce125a8462e99890cd15459a643d78abcf93c4db
Samreen Malla <samreen.malla@gmail.com>
2017-12-20 15:30:38 +0545

    comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
 	belongs_to :user
-	has _many :comments
+	has_many :comments
 	mount_uploader :photo, PhotoUploader
 
 	validates :photo, :description, :user_id, presence: true
