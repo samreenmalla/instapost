5ed96859d76464d0bac5957ceb74972a328700d4
Samreen Malla <samreen.malla@gmail.com>
2017-12-20 14:37:15 +0545

    single post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,10 @@ class PostsController < ApplicationController
   @posts = Post.all.order('created_at DESC')
   end
 
+  def show
+  @post = Post.find(params[:id])
+end
+
 	def new
 		@post = Post.new
 
